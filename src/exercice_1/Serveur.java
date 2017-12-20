aa7ad7eb63cc25fd4bbe9bf5981b631c13fa2ba4
Unknown <loredana.bernard.etu@univ-lemans.fr>
2017-12-20 11:34:52 +0100

    Classe serveur

--- src/exercice_1/Serveur.java
+++ src/exercice_1/Serveur.java
@@ -1,10 +1,26 @@
 package exercice_1;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class Serveur {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			DatagramSocket s = new DatagramSocket(6789);
+			byte[] data = new byte[100];
+			DatagramPacket paquet = new DatagramPacket(data, data.length);
+			while (true) {
+				s.receive(paquet); // Reception du paquet
+				String st = new String(paquet.getData(),0,paquet.getLength());
+				System.out.println("J'ai reçu " + st);				
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 }
