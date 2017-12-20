dd77e259cf09d5bc307632c35dff20a8a3c0f38d
Unknown <loredana.bernard.etu@univ-lemans.fr>
2017-12-20 11:26:01 +0100

    Classe client

--- src/exercice_1/Client.java
+++ src/exercice_1/Client.java
@@ -1,9 +1,25 @@
 package exercice_1;
 
-public class Client {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
 
+public class Client {
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			DatagramSocket s = new DatagramSocket();
+			byte[] data = args[0].getBytes();
+			InetSocketAddress sa = new InetSocketAddress("localhost", 6789);
+			DatagramPacket paquet = new DatagramPacket(data,data.length,sa);	// Creation paquet de données
+			s.send(paquet);			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 
 	}
 
