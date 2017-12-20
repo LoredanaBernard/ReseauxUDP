420a1bc5995149da02e56261549d5e2aa79f0e19
Unknown <loredana.bernard.etu@univ-lemans.fr>
2017-12-20 11:50:04 +0100

    Creation exercice 2

---
+++ src/balayae_ports/Balai.java
@@ -0,0 +1,16 @@
+package balayae_ports;
+
+public class Balai {
+
+	private static final int PORT_MIN = 0;
+	private static final int PORT_MAX = 0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=PORT_MIN; i<= PORT_MAX ; i++) {
+			
+		}
+	}
+
+}
